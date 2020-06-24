01191369b0f9b64263d5fbb916c3599058646959
alwaysresist <44613719+alwaysresist@users.noreply.github.com>
2020-06-25 02:11:07 +0300

    1

--- Lab5_/Form1.cs
+++ Lab5_/Form1.cs
@@ -13,8 +13,8 @@ namespace Lab5_
         PictureBox workpb;//рабочее поле PictureBox - поле на которой будет рабочий Таракан
         List<Cockroach> LC;//Список для хранения созданных Тараканов
         List<PictureBox> PB;//Список для хранения созданных объектов PictureBox
-        List<PictureBox> workfields;
-        List<Cockroach> workbugs;
+        List<PictureBox> workField;
+        List<Cockroach> workAction;
         Cockroach cockroach;
         Random rnd = new Random();
         int AlgStep; //текущая комманда
@@ -24,8 +24,8 @@ namespace Lab5_
             InitializeComponent();
             LC = new List<Cockroach>();
             PB = new List<PictureBox>();
-            workfields = new List<PictureBox>();
-            workbugs = new List<Cockroach>();
+            workField = new List<PictureBox>();
+            workAction = new List<Cockroach>();
             Random rnd = new Random();
         }
         public void RePaint(Cockroach c, PictureBox p)
@@ -37,7 +37,7 @@ namespace Lab5_
         public void Show(Cockroach c, PictureBox p, Panel owner)
         {
             RePaint(c, p);
-            owner.Controls.Add(p);// добавляем PictureBox к элементу Panel
+            owner.Controls.Add(p);//добавляем PictureBox к элементу Panel
         }
 
         private void IMouseDown(object sender, MouseEventArgs e)
@@ -51,15 +51,15 @@ namespace Lab5_
                 int k = PB.IndexOf(sender as PictureBox);//запоминаем номер нажатого компонента PictureBox
                 workpb = sender as PictureBox;//объявляем его рабочим
                 workCockroach = LC[k];//по найденному номеру находим Таракана в списке
-                if (!workfields.Any())
+                if (!workField.Any())
                 {
-                    workbugs.Add(workCockroach);
-                    workfields.Add(workpb);
+                    workAction.Add(workCockroach);
+                    workField.Add(workpb);
                 }
-                else if (!workfields.Contains(workpb))
+                else if (!workField.Contains(workpb))
                 {
-                    workbugs.Add(workCockroach);
-                    workfields.Add(workpb);
+                    workAction.Add(workCockroach);
+                    workField.Add(workpb);
                 }
             }
         }
@@ -78,7 +78,7 @@ namespace Lab5_
         {
             int dx = rnd.Next(Field.Width);
             int dy = rnd.Next(Field.Height);
-            cockroach = new Cockroach(new Bitmap("tarakan.png"));
+            cockroach = new Cockroach(new Bitmap("cockroach.png"));
             cockroach.Image.Tag = "1";
             cockroach.newcoordinates(dx, dy);
             PictureBox p = new PictureBox();
@@ -91,8 +91,8 @@ namespace Lab5_
             workCockroach = cockroach;
             workpb = p;
             ClearWorkItems();
-            workfields.Add(workpb);
-            workbugs.Add(workCockroach);
+            workField.Add(workpb);
+            workAction.Add(workCockroach);
         }
 
         private void Field_DragEnter(object sender, DragEventArgs e)
@@ -118,9 +118,9 @@ namespace Lab5_
             Point pointDrag = (Point)picture.Tag;
             //вычисляем и устанавливаем Location для PictureBox в Panel
             picture.Location = new Point(pointDrop.X - pointDrag.X + picture.Location.X, pointDrop.Y - pointDrag.Y + picture.Location.Y);
-            workbugs[0].X = picture.Location.X;
-            workbugs[0].Y = picture.Location.Y;
-            workfields[0].Location = picture.Location;
+            workAction[0].X = picture.Location.X;
+            workAction[0].Y = picture.Location.Y;
+            workField[0].Location = picture.Location;
             picture.Parent = panel;
         }
 
@@ -132,21 +132,21 @@ namespace Lab5_
             }
             else//выполнение команды из списка
             {
-                for (int i = 0; i < workbugs.Count; ++i)
+                for (int i = 0; i < workAction.Count; ++i) 
                 {
                     string s = (string)Algorithm.Items[AlgStep];
                     Algorithm.SetSelected(AlgStep, true);
                     if (s == "Step")
-                        workbugs[i].Step();
+                        workAction[i].Step();
                     if (s == "Up")
-                        workbugs[i].ChangeTrend(s);
+                        workAction[i].ChangeTrend(s);//ChangeTrend описан в отдельном классе для каждой кнопки направления движения 
                     if (s == "Left")
-                        workbugs[i].ChangeTrend(s);
+                        workAction[i].ChangeTrend(s);
                     if (s == "Right")
-                        workbugs[i].ChangeTrend(s);
+                        workAction[i].ChangeTrend(s);
                     if (s == "Down")
-                        workbugs[i].ChangeTrend(s);
-                    RePaint(workbugs[i], workfields[i]);
+                        workAction[i].ChangeTrend(s);
+                    RePaint(workAction[i], workField[i]);
                 }
                 AlgStep++;
             }
@@ -165,10 +165,10 @@ namespace Lab5_
 
         private void DeleteButton_Click(object sender, EventArgs e)//кнопка удаления таракана
         {
-            while (workfields.Count > 0)
+            while (workField.Count > 0)
             {
-                workfields[0].Dispose();
-                workfields.RemoveAt(0);
+                workField[0].Dispose();
+                workField.RemoveAt(0);
             }
             if (workpb != null)
             {
@@ -179,8 +179,8 @@ namespace Lab5_
 
         private void ClearWorkItems()
         {
-            workbugs.Clear();
-            workfields.Clear();
+            workAction.Clear();
+            workField.Clear();
         }
 
         private void UpButton_Click(object sender, EventArgs e)
